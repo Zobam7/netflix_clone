5f5a41b096b8a9d1397c0981e6d782f5e1da5b79
Zobam <zobamokoli7@gmail.com>
2022-07-14 14:19:40 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+
+function Navbar() {
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    try {
+      await logOut();
+      navigate(`/`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="flex items-center justify-between p-4 z-[100] absolute w-full">
+      <Link to="/">
+        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+          NETFLIX
+        </h1>
+      </Link>
+      {user?.email ? (
+        <div className="">
+          <Link to="/account">
+            <button className="text-white pr-4">Accout</button>
+          </Link>
+
+          <button
+            onClick={handleLogOut}
+            className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white"
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div className="">
+          <Link to="/login">
+            <button className="text-white pr-4">Sign In</button>
+          </Link>
+          <Link to="/signUp">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Navbar;
